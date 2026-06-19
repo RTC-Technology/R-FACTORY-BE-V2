3716c80176ef2e39d6bdba97f23a8ff87369753f
Admin <Admin@DESKTOP-40H717B>
2026-06-19 14:19:08 +0700

    add apiresponse

---
+++ R_FACTORY_BE/Common/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace R_FACTORY_BE.Common;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+    public T? Data { get; set; }
+    public object? Errors { get; set; }
+}
---
+++ R_FACTORY_BE/Common/ApiResponseFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace R_FACTORY_BE.Common;
+
+public static class ApiResponseFactory
+{
+    public static IActionResult Success<T>(
+      T data,
+      string? message = null,
+      HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        return new ObjectResult(new ApiResponse<T>
+        {
+            Success = true,
+            StatusCode = (int)statusCode,
+            Message = message,
+            Data = data
+        })
+        {
+            StatusCode = (int)statusCode
+        };
+    }
+
+    public static IActionResult Fail(
+        string message,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest,
+        object? errors = null)
+    {
+        return new ObjectResult(new ApiResponse<object>
+        {
+            Success = false,
+            StatusCode = (int)statusCode,
+            Message = message,
+            Errors = errors
+        })
+        {
+            StatusCode = (int)statusCode
+        };
+    }
+}
